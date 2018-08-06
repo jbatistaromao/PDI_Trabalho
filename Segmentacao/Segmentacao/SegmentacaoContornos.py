3ddda3c33c2cbbf92f7d2a50d5d48513a68bdd16
adrie_000 <adrie_000@ADRIEL>
2018-08-05 23:28:08 -0300

    Contorno Finalizado

--- Segmentacao/Segmentacao/SegmentacaoContornos.py
+++ Segmentacao/Segmentacao/SegmentacaoContornos.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import warnings
 
 #funcao identifica se ha uma pilula circular 
 #retorna coordenada x e y do ponto central e o raio do circulo
@@ -8,27 +9,58 @@ from matplotlib import pyplot as plt
 def contemPilulaCircular(imageUrl):
     #carrega e trata a imagen antes de aplicar a transformada de hough
 
-    img = cv2.imread(imageUrl,0)
-    #img = cv2.blur(img,(10,10))
-
-    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-
-    
-    
+   
  
     #checa se e necessario ou nao aplicar o blur
 
-    aux = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=100,param2=30,minRadius=0,maxRadius=0)
-    if(len(aux[0]) > 2):
-        #gera o vetor de circulos achados na imagem
-        img = cv2.medianBlur(img,5)  
-        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=100,param2=30,minRadius=0,maxRadius=0)
-    else:
+    continuar = True
+    parameter = 1
+
+
+    while continuar == True:
+        img = cv2.imread(imageUrl,0)
+        img = cv2.medianBlur(img,parameter)
+        cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
         circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
                             param1=100,param2=30,minRadius=0,maxRadius=0)
+
+ 
+        if circles is None:
+            parameter = parameter - 2
+            img = cv2.imread(imageUrl,0)
+            img = cv2.medianBlur(img,parameter)
+            cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+            circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=100,param2=30,minRadius=0,maxRadius=0)
+            
+            break
+
+        if len(circles[0]) > 2:
+            parameter = parameter + 2
+        elif len(circles[0]) == 2:
+            #distancia entre os circulos
+            d = abs(int(circles[0][0][0]) - int(circles[0][1][0]))
+            #raio dos circulos
+            r1 = circles[0][0][2]
+            r2 = circles[0][1][2]
+
+            if ~(d - d*0.05 <= r1+r2 <= d + d*0.05):
+                parameter = parameter + 2
+            else:
+               break
+        else:
+            continuar = False
+
+        if parameter > 15:
+            print 'Nao ha pilulas circulares'
+            cv2.imshow('detected circles',cimg)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            return 0,0,0
+
+
 
     circles = np.uint16(np.around(circles))
     for i in circles[0,:]:
@@ -37,9 +69,7 @@ def contemPilulaCircular(imageUrl):
         # draw the center of the circle
         cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-    print 'qtd circulos:'
-    print len(circles[0])
-    print circles
+
 
     
   
@@ -63,14 +93,14 @@ def contemPilulaCircular(imageUrl):
             return 0,0,0
 
     elif len(circles[0]) == 2:
+
         #distancia entre os circulos
-        d = abs(circles[0][0][0] - circles[0][1][0])
-        
+        d = abs(int(circles[0][0][0]) - int(circles[0][1][0]))
         #raio dos circulos
         r1 = circles[0][0][2]
         r2 = circles[0][1][2]
-
-        if(d - d*0.05 <= r1+r2 >= d + d*0.05):
+        
+        if(d - d*0.05 <= r1+r2 <= d + d*0.05):
             print 'possui duas pilulas circulares'
 
             cv2.imshow('detected circles',cimg)
@@ -97,5 +127,5 @@ def contemPilulaCircular(imageUrl):
         cv2.destroyAllWindows()
            
         return 0,0,0
-contemPilulaCircular('imagens remedio\\Medium cut\\5036_lg.jpg')
+contemPilulaCircular('imagens remedio\\Medium cut\\5014_lg.jpg')
 
